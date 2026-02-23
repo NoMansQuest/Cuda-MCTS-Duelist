0c58b0b4d15d427f938d43ce8993a0e82a223693
Nasser GHOSEIRI <nasser.ghoseiri@gmail.com>
2026-02-23 11:20:58 +0100

    Updated gameboard print output

--- src/game_helpers.cpp
+++ src/game_helpers.cpp
@@ -2,9 +2,11 @@
 
 void print_game_board(std::array<int, game_board_size> game_board)
 {
+    std::cout << "      1 2 3 4 5 6 7" << std::endl;
+    std::cout << "     --------------" << std::endl;
     for (auto row = 0; row < game_board_rows; row++)
     {
-        std::cout << "    "; // Add a bit of a left-padding
+        std::cout << "  " << (row + 1) << " | "; // Add a bit of a left-padding
         for (auto col = 0; col < game_board_columns; col++)
         {
             auto slot_value = game_board[TO_LINEAR(row, col)];
